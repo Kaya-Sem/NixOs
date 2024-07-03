c0215c2063992ad95ca3064cda9f3cfb28c9c5eb
Kaya-Sem <kayasemvc@gmail.com>
2024-07-03 16:01:30 +0200

    Rebuild NixOS and apply changes

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,7 @@
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
+    android-studio
     luajitPackages.luarocks-nix
     jdk
     android-tools
